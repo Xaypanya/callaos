46c5a4439dd39564da391f25925c63db6e6066b3
Xaypanya Phongsa <dackogan@gmail.com>
2023-02-03 01:17:57 +0700

    fix for mobile size and add icon

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,19 @@
 import axios from "axios"
 
+//dev
+// const server = "http://127.0.0.1:4000"
+//production
+const server = "http://192.168.100.9:4000"
+
 export const GetOneAPI = async (path, uuid) => {
 
-   const response = await axios.get(`http://localhost:4000${path}/${uuid}`)
+   const response = await axios.get(`${server}${path}/${uuid}`)
    return response
 }
 
 export const GetAllAPI = async (path) => {
 
-        const response = await axios.get(`http://localhost:4000${path}`)
+        const response = await axios.get(`${server}${path}`)
         return response
 
  }
